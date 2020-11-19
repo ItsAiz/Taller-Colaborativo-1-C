64b337b8110af41121acf3f6ed28165ecb3a1218
ItsAiz <73964837+ItsAiz@users.noreply.github.com>
2020-11-19 15:43:02 -0500

    Menu incluido

--- main.c
+++ main.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+void mainMenu(){
+    char *menu="\n---------------Taller---------------\n "
+               "1.Convertir nombre propio\n "
+               "2.Contar cantidad de palabras \n "
+               "3.Encriptar cadena \n "
+               "4.Desencriptar cadena \n 5.Llenar caracteres por izquierda o derecha \n "
+               "6.Borrar caracteres de una cadena \n 7.Interseccion \n"
+               " 8.Diferencia entre dos cadenas \n"
+               " 9.Borrar aracteres izquierda o derecha \n"
+               " 10.Salir \n"
+               "-----------------------------------\n";
+    int option;
+    do{
+        system("cls");
+        printf("%s",menu);
+        scanf("%d",&option);
+        fflush(stdin);
+        switch (option) {
+            case 1 :;
+            break;
 
+            case 2 :;
+            break;
+
+            case 3 :;
+            break;
+
+            case 4 :;
+            break;
+
+            case 5 :;
+            break;
+
+            case 6 :;
+            break;
+            case 7 :;
+            break;
+            case 8 :;
+            break;
+            case 9 :;
+            break;
+        }
+    }while(option!=10);
+    printf("Cerrando ....");
+}
 int main() {
-    printf("Rama ivan!\n");
+    mainMenu();
     return 0;
 }
