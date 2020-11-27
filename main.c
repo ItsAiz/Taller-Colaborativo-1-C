3c49c14e5478ca84fcee6df7c08c918efd3d1125
unknown <freddy.ojeda@uptc.edu.co>
2020-11-26 19:08:01 -0500

    Segundo ejercicio hecho

--- main.c
+++ main.c
@@ -125,6 +125,39 @@ void wordCounter(){
 }
 
 void fillCharacterRL(){
+    char message[100];
+    char *aux;
+    char character[10];
+    int nTimes = 0;
+    int direction = 0;
+
+    printf("---------------Llenar caracteres por izquierda o derecha---------------\n");
+
+    printf("Digite un mensaje:\n");
+    gets(message);
+
+    printf("Digite el caracter que desea concatenar:\n");
+    scanf("%c",&character);
+
+    printf("Digite el numero de veces que desea concatenar el caracter:\n");
+    scanf("%d",&nTimes);
+
+    printf("Digite 1 para insertar por derecha o 2 para insertar por izquierda: \n");
+    scanf("%d", &direction);
+
+    fflush(stdin);
+
+    aux=character;
+
+    strncat(aux,character,(nTimes-1));
+
+    if (direction==1){
+        strcat(message,aux);
+        printf("%s\n",message);
+    }else{
+        strcat(aux,message);
+        printf("%s\n",aux);
+    }
 
 }
 
@@ -137,7 +170,8 @@ void mainMenu(){
                "1.Convertir nombre propio\n "
                "2.Contar cantidad de palabras \n "
                "3.Encriptar cadena \n "
-               "4.Desencriptar cadena \n 5.Llenar caracteres por izquierda o derecha \n "
+               "4.Desencriptar cadena \n "
+               "5.Llenar caracteres por izquierda o derecha \n "
                "6.Borrar caracteres de una cadena \n 7.Interseccion \n"
                " 8.Diferencia entre dos cadenas \n"
                " 9.Borrar caracteres izquierda o derecha \n"
@@ -170,7 +204,7 @@ void mainMenu(){
 
             case 5 :
                 fillCharacterRL();
-                system("coming soon");
+                system("pause");
                 break;
 
             case 6 :deleteCharacter();
