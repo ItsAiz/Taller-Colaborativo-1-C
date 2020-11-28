58b6a2be4f41c321af0219dc4b163099240acde1
unknown <freddy.ojeda@uptc.edu.co>
2020-11-28 10:01:29 -0500

    Parte mejorada

--- main.c
+++ main.c
@@ -1,105 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
-int encrypted(char string[]){
-
-}
-void encryptString(){
-    char string[4];
-    printf("Digite lo que quiere encriptar \n");
-    scanf("%s",&string);
-}
-
-int coincidence(char character,const char *stringTwo){
-    int accumulator=0;
-    for (int i = 0; i <strlen(stringTwo); ++i) {
-        if(character==stringTwo[i]){
-            accumulator++;
-        }
-    }
-    return accumulator > 0 ? 1 : 0;
-}
-void deleteCharactersLeft(char *string,const char *stringTwo){
-    int accumulator=0,k=0;
-    char res[100];
-    for (int j = 0; j <strlen(string)-2 ; ++j) {
-        if(coincidence(string[j],stringTwo)==1){
-            string[j]=' ';
-            accumulator++;
-        }else{
-            break;
-        }
-    }
-    if(accumulator>0){
-        for (int i = 0; i <strlen(string); ++i) {
-            if(string[i]!=' '){
-                res[k]=string[i];
-                k++;
-            }
-        }
-        res[k]='\0';
-        printf("%s\n",res);
-    }else{
-        printf("%s \n",string);
-    }
-}
-void deleteCharactersRight(char *string,const char *stringTwo){
-    for (int i = strlen(string)-2; i >= 0; --i) {
-        if(coincidence(string[i],stringTwo)==1){
-            string[i]=' ';
-        }else{
-            break;
-        }
-    }
-    printf("%s\n",string);
-}
-void deleteCharactersRightOrLeft(){
-    int option=0;
-    char string[100];
-    char stringTwo[100];
-    do{
-        printf("---------------Borrar caracteres izquierda o derecha---------------\n");
-        printf("Digite una cadena: \n");
-        fgets(string,100,stdin);
-        printf("Digite una segunda cadena: \n");
-        fgets(stringTwo,100,stdin);
-        printf("Digite 1.Derecha 2.Izquierda\n");
-        scanf("%d",&option);
-        fflush(stdin);
-    }while (strlen(string)==1 || strlen(stringTwo)==1 || option!=1 || option!=2);
-    if(option==1){
-        deleteCharactersRight(string,stringTwo);
-    }else if(option==2){
-        deleteCharactersLeft(string,stringTwo);
-    }
-}
-void removeCharacter(const char *string, char character){
-    int j=0;
-    char stringTw[strlen(string)];
-    for (int i = 0; i < strlen(string); ++i) {
-        if(string[i]!=character){
-            stringTw[j]=string[i];
-            j++;
-        }
-    }
-    stringTw[j]='\0';
-    printf("La cadena sin el caracter %c es :%s \n",character,stringTw);
-}
-void deleteCharacter(){
-    char string[100];
-    char character;
-    do{
-        printf("---------------Borrar caracter de una cadena---------------\n");
-        printf("Digite una cadena: \n");
-        fgets(string,100,stdin);
-        printf("Digite el caracter que desea borrar: \n");
-        scanf("%c",&character);
-        fflush(stdin);
-    }while(strlen(string)==1 || character==' ');
-    printf("Cadena inicial: %s\n ",string);
-    removeCharacter(string, character);
-}
 
 void wordCounter(){
     int counter = 0;
@@ -137,7 +38,7 @@ void fillCharacterRL(){
     gets(message);
 
     printf("Digite el caracter que desea concatenar:\n");
-    scanf("%c",&character);
+    scanf("%c",character);
 
     printf("Digite el numero de veces que desea concatenar el caracter:\n");
     scanf("%d",&nTimes);
@@ -162,8 +63,8 @@ void fillCharacterRL(){
 }
 
 void messagesDifference(){
-    int size1=0;
-    int size2 = 0;
+    int size1;
+    int size2;
     short i = 0;
     char message1[100];
     char message2[100];
@@ -212,7 +113,7 @@ void mainMenu(){
         scanf("%d",&option);
         fflush(stdin);
         switch (option) {
-            case 1 :;
+            case 1 :
                 system("pause");
                 break;
 
@@ -222,11 +123,10 @@ void mainMenu(){
                 break;
 
             case 3 :
-                encryptString();
                 system("pause");
                 break;
 
-            case 4 :;
+            case 4 :
                 system("pause");
                 break;
 
@@ -235,11 +135,11 @@ void mainMenu(){
                 system("pause");
                 break;
 
-            case 6 :deleteCharacter();
+            case 6 :
                 system("pause");
                 break;
 
-            case 7 :;
+            case 7 :
                 system("pause");
                 break;
 
@@ -248,7 +148,7 @@ void mainMenu(){
                 system("pause");
                 break;
 
-            case 9 : deleteCharactersRightOrLeft();
+            case 9 :
                 system("pause");
                 break;
         }
@@ -256,8 +156,6 @@ void mainMenu(){
     printf("Cerrando ...");
 }
 int main() {
-    printf("Soy Freddy");
-    printf("Y tambien Esteban");
     mainMenu();
     return 0;
 }
